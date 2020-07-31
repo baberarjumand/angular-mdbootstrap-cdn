6b0f6ea3a94d33aea274715119ed2822316a672a
baberarjumand <baber.arjumand@gmail.com>
2020-07-31 17:46:48 +0300

    added mdbootstrap via cdn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-mdbootstrap-cdn';
+  showPicsFlag = false;
+
+  constructor() {}
+
+  showPics() {
+    this.showPicsFlag = !this.showPicsFlag;
+  }
 }
